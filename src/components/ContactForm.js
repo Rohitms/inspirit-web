a1934f4a4fcdc1516478178eeeaeace4892ac835
RohitMS <rohithmsasi@gmail.com>
2019-06-18 21:01:55 +0530

    form fix

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,7 +4,23 @@ export default class ContactForm extends React.Component {
   render() {
     return (
       <div className="Contactform">
-        <input type="hidden" name="form-name" value="contact" />
+        <form name="contact" method="POST">
+          <input type="hidden" name="form-name" value="contact" />
+          <div className="FormTitle">Name</div>
+          <input type="text" name="name" placeholder="" />
+          <div className="FormTitle">Email</div>
+          <input type="email" name="email" />
+          <div className="FormTitle">Number</div>
+          <input type="tel" name="number" />
+          <div className="FormTitle">Subject</div>
+          <input type="text" name="subject" />
+          <div className="FormTitle">Message</div>
+          <textarea type="text" name="message" />
+
+          <div className="FormTitle ">
+            <input className="cfbutton" type="submit" />
+          </div>
+        </form>
       </div>
     )
   }
